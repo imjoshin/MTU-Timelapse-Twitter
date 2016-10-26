c20c6883b3936ff3e06972d4835e9c275a255e07
Josh <jocjohns@mtu.edu>
2016-10-25 22:05:46 -0400

    Now gets weather. Finds correct file.

--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
 TWEETINTRO = [
     "Today it is",
     "It is currently",
-    "Right now it's", 
+    "Right now it's",
     "Currently it's"
 ]
 
-TWEET = "%s %d degrees and %s in Houghton, Michigan!"
+TWEET = "%s %d degrees with a %s in Houghton, Michigan!"
--- tweet.py
+++ tweet.py
@@ -1,11 +1,19 @@
-import tweepy, random
+import tweepy, random, urllib, json, time, os
 import settings, auth
 
 def main():
     api = getAPI()
-    filename = "../MTU-Timelapse/cam/10-25-16/12-00.jpg"
-    tweet = settings.TWEET % (random.choice(settings.TWEETINTRO), 65, "cloudy")
-    api.update_with_media(filename, status=tweet)
+
+    day = time.strftime("%m-%d-%y")
+
+    for i in range(0, 13):
+        filename = "../MTU-Timelapse/cam/%s/12-%s.jpg" % (day, str(i * 5).zfill(2))
+        if(os.path.isfile(filename)):
+            break
+
+    degrees, weather = getWeather()
+    tweet = settings.TWEET % (random.choice(settings.TWEETINTRO), degrees, weather)
+    #api.update_with_media(filename, status=tweet)
     print "Posted tweet '%s' with image '%s'" % (tweet, filename)
 
 
@@ -15,6 +23,14 @@ def getAPI():
     api = tweepy.API(authorization)
     return api
 
+def getWeather():
+    url = "http://api.openweathermap.org/data/2.5/weather?q=Houghton&APPID=" + auth.WEATHERKEY
+    response = urllib.urlopen(url)
+    data = json.loads(response.read())
+    kelvin = data['main']['temp']
+    degrees = kelvin * 9/5 - 459.67
+    weather = data['weather'][0]['description']
+    return degrees, weather
 
 if __name__ == "__main__":
     main()
